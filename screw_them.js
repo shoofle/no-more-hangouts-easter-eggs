b26889f07c0d61064fef1e2faa2804f51532fa37
shoofle <xsigma@gmail.com>
2015-01-31 14:12:32 -0500

    run screw_them in the hangouts iframes

---
+++ screw_them.js
@@ -0,0 +1,18 @@
+var config = {childList: true, subtree: true, attributes: false, characterData: false}
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    length = mutation.addedNodes.length;
+    for (var i=0; i<length; i++) {
+      var el = mutation.addedNodes[i];
+      if (!('tagName' in el)) { continue; }
+      var sub = el.getElementsByTagName("img");
+      for (var j=0; j<sub.length; j++) {
+        if (sub[j].src.indexOf("chat/babble/ee/anim") != -1) {
+          sub[j].remove();
+        }
+      }
+    }
+  });
+});
+
+observer.observe(document.body, config);
